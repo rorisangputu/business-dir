cc43c5b5a1482fc9ff51c1927d4f8eeec85dfb8e
Rorisang Putu <rorisangputu@gmail.com>
2024-07-04 01:17:48 +0200

    creating query search params

--- app/api/googlePlaceApi/route.tsx
+++ app/api/googlePlaceApi/route.tsx
@@ -4,9 +4,11 @@ const BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
 const API_KEY = process.env.PLACES_API;
 
 export async function GET(request: any) {
+    const { searchParams }: any = new URL(request.url);
+    const q = searchParams.get('q');
 
     const res = await fetch(
-        BASE_URL + "query=restaurants%20in%20Johannesburg&key=" + API_KEY,
+        BASE_URL + "query=" + q + "&key=" + API_KEY,
         {
             headers: {
                 "Content-Type": "application/json",
