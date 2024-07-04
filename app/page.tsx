cf873734f3fded89b104ceebde2f4a6a470b8204
Rorisang Putu <rorisangputu@gmail.com>
2024-07-04 15:19:38 +0200

    fetching place list from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
 import Hero from "@/components/Hero";
 import Header from "@/components/Header";
-
 import Image from "next/image";
 import PlaceList from "@/components/PlaceList";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const getPlaceList = async () => {
+    const res = await fetch("/api/google-place-api?q=Hotels in Rustenburg");
+    const data = await res.json();
+
+    //console.log(data.response.results);
+    setPlaceList(data.response.results);
+  }
+  const [placeList, setPlaceList] = useState([]);
+
+  useEffect(() => {
+    getPlaceList();
+  }, []);
+
   return (
     <>
       <Header />
       <Hero />
-      <PlaceList />
+      {placeList ? <PlaceList placeList={placeList} /> : null}
     </>
   );
 }
--- components/PlaceList.tsx
+++ components/PlaceList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const PlaceList = () => {
+const PlaceList = ({ placeList }) => {
     return (
         <div className='px-[10px] md:px[120px] mt-7'>
             <h1 className='text-[20px] font-semibold'>Search Result:</h1>
